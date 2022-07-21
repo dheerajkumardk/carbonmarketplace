ebe53369f13a9d83d797f9e49f08995bfdce73e2
vijay-element <vijay.wankhede@elementzerolabs.com>
2022-07-21 20:50:19 +0530

    EBLOCK-124: Upgrade the pragma to 0.8.10

--- contracts/Membership/MembershipTrader.sol
+++ contracts/Membership/MembershipTrader.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
