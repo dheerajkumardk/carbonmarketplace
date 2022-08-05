e77c902aed1edcf8f6b3b6693b18e0ccc50ba4d6
dheeraj kumar <dyaduwanshi0@gmail.com>
2022-08-05 18:01:28 +0530

    element-token

---
+++ contracts/Airdrop.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./ELEMENT.sol";
+
+
+contract Airdrop is Ownable {
+    using SafeMath for uint256;
+
+    address public tokenAddr;
+
+   
+}
--- contracts/ELEMENT.sol
+++ contracts/ELEMENT.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 
 import "./ETHToken.sol";
 
-contract GEMSToken is ERC20 {
-    constructor() ERC20("CARBON elements", "element") {
+contract ElementToken is ERC20 {
+    constructor() ERC20("Element", "ELEMENT") {
         _mint(msg.sender, 1000000000 * 10**18); // Mints 1B CARBON elements
     }
 }
